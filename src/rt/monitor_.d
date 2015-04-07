21fb3efd8bcfe0b15be82ce5e99cf3528973d8a4
Johan Engelen <j.b.c.engelen@alumnus.utwente.nl>
2015-04-07 18:25:00 +0200

    Fix linking on Windows: explicit mention of libcmt conflicts with default linking with libcmtd.

--- src/rt/monitor_.d
+++ src/rt/monitor_.d
@@ -55,8 +55,8 @@ private
         }
         else version (CRuntime_Microsoft)
         {
-            pragma(lib, "libcmt.lib");
-            pragma(lib, "oldnames.lib");
+            // pragma(lib, "libcmt.lib");
+            // pragma(lib, "oldnames.lib");
         }
         import core.sys.windows.windows;
 
